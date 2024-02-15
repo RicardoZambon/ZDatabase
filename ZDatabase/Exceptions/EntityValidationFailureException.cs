b6925394b28a3692406501e3d182fa305970d57b
Ricardo Zambon <ricardo.zambon@live.com>
2024-02-15 08:49:55 -0300

    feat: Add validation methods.

---
+++ ZDatabase/Exceptions/EntityValidationFailureException.cs
@@ -0,0 +1,33 @@
+﻿using ZDatabase.Validations;
+
+namespace ZDatabase.Exceptions
+{
+    /// <summary>
+    /// Represents when the repository fails during an entity validation.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the key.</typeparam>
+    /// <seealso cref="ZDatabase.Exceptions.ValidationFailureException" />
+    public class EntityValidationFailureException<TKey> : ValidationFailureException
+        where TKey : struct
+    {
+        /// <summary>
+        /// Gets the entity key.
+        /// </summary>
+        /// <value>
+        /// The entity key.
+        /// </value>
+        public TKey EntityKey { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EntityValidationFailureException{TKey}"/> class.
+        /// </summary>
+        /// <param name="entityName">The entity name.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="validationResult">The validation result.</param>
+        public EntityValidationFailureException(string entityName, TKey key, ValidationResult validationResult)
+            : base(validationResult, $"Entity '{entityName} ({key})' has validation problems.")
+        {
+            EntityKey = key;
+        }
+    }
+}
---
+++ ZDatabase/Exceptions/ValidationFailureException.cs
@@ -0,0 +1,39 @@
+﻿using ZDatabase.Validations;
+
+namespace ZDatabase.Exceptions
+{
+    /// <summary>
+    /// Represents when the repository fails during validation.
+    /// </summary>
+    /// <seealso cref="System.Exception" />
+    public class ValidationFailureException : Exception
+    {
+        /// <summary>
+        /// Gets the validation result.
+        /// </summary>
+        /// <value>
+        /// The validation result.
+        /// </value>
+        public ValidationResult ValidationResult { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ValidationFailureException"/> class.
+        /// </summary>
+        /// <param name="validationResult">The validation result.</param>
+        /// <param name="message">The message.</param>
+        public ValidationFailureException(ValidationResult validationResult, string? message)
+            : base(message)
+        {
+            ValidationResult = validationResult;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ValidationFailureException"/> class.
+        /// </summary>
+        /// <param name="validationResult">The validation result.</param>
+        public ValidationFailureException(ValidationResult validationResult)
+            : this(validationResult, $"Entity has validation problems.")
+        {
+        }
+    }
+}
