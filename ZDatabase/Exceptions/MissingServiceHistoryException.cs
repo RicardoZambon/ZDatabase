f0345792962163156a0b7298a7af3cf3a73f152b
Ricardo Zambon <samuraizambon@outlook.com>
2026-06-11 23:56:21 -0300

    fix: fix warnings

--- ZDatabase/Exceptions/MissingServiceHistoryException.cs
+++ ZDatabase/Exceptions/MissingServiceHistoryException.cs
@@ -1,7 +1,7 @@
 ﻿namespace ZDatabase.Exceptions
 {
     /// <summary>
-    /// Represents errors that occurs when the database context can not find any <see cref="ZDatabase.Entities.Audit.ServicesHistory{TUsers, TUsersKey}"/>.
+    /// Represents errors that occurs when the database context can not find any <see cref="ZDatabase.Entities.Audit.ServicesHistory{TServicesHistory, TOperationsHistory, TUsers, TUsersKey}"/>.
     /// </summary>
     /// <seealso cref="System.Exception" />
     public class MissingServiceHistoryException
